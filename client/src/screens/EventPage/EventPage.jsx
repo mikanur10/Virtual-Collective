9af150c239a66b2dc71e444ed4426e31280ba573
Elijah Heyward <elijah.heyward76@gmail.com>
2020-10-30 16:11:08 -0400

    eh-created carousel and universal layout

---
+++ client/src/screens/EventPage/EventPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './EventPage.css'
+import EventCarousel from '../../components/Carousel/EventCarousel'
+import UniversalLayout from '../../components/shared/UniversalLayout/UniversalLayout'
+
+
+
+function EventPage(props) {
+  return (
+    <UniversalLayout>
+      <div className="art-header">
+        <h3>ARTS</h3>
+      </div>
+      <div className="carousel">
+        <EventCarousel />
+      </div>
+   
+    </UniversalLayout>
+
+  );
+}
+
+export default EventPage;

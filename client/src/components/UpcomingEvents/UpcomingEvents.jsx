ae28de86babd661e2bf5edc3890575752c42c490
Mika Nur <mikanur@Mikas-MBP.fios-router.home>
2020-11-02 12:26:28 -0500

    MN-stylingOnUpcomingEvents

--- client/src/components/UpcomingEvents/UpcomingEvents.jsx
+++ client/src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./UpcomingEvents.css";
 import { getEvents } from "../../services/events";
-import { useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 const UpcomingEvents = () => {
-    const params = useParams();
+  const params = useParams();
   const [events, setEvents] = useState({});
   const [isLoaded, setLoaded] = useState(false);
 
@@ -21,24 +21,24 @@ const UpcomingEvents = () => {
   if (!isLoaded) {
     return <h1>Loading...</h1>;
   }
-  const filterEvents = events.filter(event =>
-    event.category === "Arts"
-    );
+  const filterEvents = events.filter((event) => event.category === "Arts");
   return (
-      <div className="upcoming-events">
-      {
-        filterEvents.map(
-            (event) => (
-                
-                <div>
-                    <img src={event.imgUrl} />
-                    <p>{event.name}</p>
-                    <p>{event.date}</p>
-                    <p>{event.time}</p>
-                    <p>{event.subCategory}</p>
-             </div>
-          )
-        )}
+    <div className="all-content">
+      <div>
+        <h3 className="upcoming-title">UPCOMING</h3>
+      </div>
+      {filterEvents.map((event) => (
+        <div className="upcoming-events">
+          <img className="image" src={event.imgUrl} />
+          <div className="info">
+            <p className="name">{event.name}</p>
+            <p className="date">
+              {event.date} @ {event.time}
+            </p>
+            <p className="subCategory">{event.subCategory}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };

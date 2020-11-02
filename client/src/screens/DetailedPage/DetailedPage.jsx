81f4c4c080cc3af8f187ee4a3c77f53713d4761b
Elijah Heyward <elijah.heyward76@gmail.com>
2020-11-02 16:48:46 -0500

    eh-mn-cm-created detailedpage+styling

--- client/src/screens/DetailedPage/DetailedPage.jsx
+++ client/src/screens/DetailedPage/DetailedPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import './DetailedPage.css';
 import UniversalLayout from "../../components/shared/UniversalLayout/UniversalLayout";
 import { getEvent } from "../../services/events";
 
@@ -17,15 +18,29 @@ function DetailedPage(props) {
   }, []);
 
   return (
-    <div id="">
-      <img class="" src={event.imgUrl} alt={event.name} />
-      <div class="">
-        <h3>{event.name}</h3>
-        <h4>({event.description})</h4>
-        <p>{event.date}</p>
-        <p>{event.time}</p>
+    <UniversalLayout>
+      <div>
+        <div>
+        <Link to="/events/arts">
+          <p className="back-button">ARTS</p>
+        </Link>
+        </div>
+          <div id="detail-page">
+
+        <img className="detail-image" src={event.imgUrl} alt={event.name} />
+        <div className="detail-container">
+          <h3 className="detail-name">{event.name}</h3>
+          <p className="descrip-title"><b>Event Description</b></p>
+          <p className="detail-descrip">{event.description}</p>
+          <p className="date-title"><b>{event.subCategory} Date & Time</b></p>
+          <p className="detail-date">{event.date} @ {event.time}</p>
+          
+        </div>
+      </div>  
       </div>
-    </div>
+
+    </UniversalLayout>
+
   );
 }
 export default DetailedPage;

ce4e1d3ca456272a5e14fce664d7f94875a0260d
Elijah Heyward <elijah.heyward76@gmail.com>
2020-11-05 16:48:36 -0500

    eh-cm-mn finished favorites functionality + css

--- client/src/components/UpcomingEvents/UpcomingEvents.jsx
+++ client/src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./UpcomingEvents.css";
-import { getEvents } from "../../services/events";
+import { getEvents, updateEvent } from "../../services/events";
 import { Link } from "react-router-dom";
 
 const UpcomingEvents = () => {
@@ -17,9 +17,29 @@ const UpcomingEvents = () => {
     fetchEvents();
   }, []);
 
+  const handleFavorite = async (eventFavorite) => {
+    console.log(eventFavorite);
+      // const updated = await updateEvent(_id, events)
+    if (eventFavorite.favorite === false) {
+      let updatedEvent = {
+        ...eventFavorite,
+        favorite: true
+      }
+      await updateEvent(eventFavorite._id, updatedEvent)
+    } else {
+      let updatedEvent = {
+        ...eventFavorite,
+        favorite: false
+      }
+      await updateEvent(eventFavorite._id, updatedEvent)
+    }
+    window.location.reload()
+  }
+
   if (!isLoaded) {
     return <h1>Loading...</h1>;
   }
+
   const filterEvents = events.filter((event) => event.category === "Arts");
 
   return (
@@ -27,11 +47,12 @@ const UpcomingEvents = () => {
       <div className="upcoming-add">
         <h3 className="upcoming-title">UPCOMING</h3>
         <Link to="/create">
-          <img
+          {/* <img
             className="add-icon"
             src="https://i.imgur.com/rYgXM9m.png"
             alt="add"
-          />
+          /> */}
+          <p className="add-icon">+</p>
         </Link>
       </div>
       {filterEvents.map((event) => (
@@ -46,13 +67,12 @@ const UpcomingEvents = () => {
             </p>
             <p className="subCategory">{event.subCategory}</p>
           </div>
-          <button className="heart-button-container">
             <img
+              onClick={() => { handleFavorite(event) }}
               className="heart"
-              src="https://i.imgur.com/dHFsXQ4.png"
+              src={event.favorite === true ? "https://i.imgur.com/95TaJ6f.png" : "https://i.imgur.com/dHFsXQ4.png"}
               alt="heart"
             />
-          </button>
         </div>
       ))}
     </div>
